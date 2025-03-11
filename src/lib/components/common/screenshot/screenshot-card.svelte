0d837fdda2a7417de3ade21bd6c0629a1d5a1ae8
daviddefi <daviddao.upwork@gmail.com>
2025-03-11 01:27:55 -0400

    Fix screenshot display with img tags and correct paths

--- src/lib/components/common/screenshot/screenshot-card.svelte
+++ src/lib/components/common/screenshot/screenshot-card.svelte
@@ -18,22 +18,29 @@
 
 <Dialog>
 	<DialogTrigger>
-		<Card
-			class="flex aspect-square flex-col justify-end bg-cover bg-center bg-no-repeat"
-			style={`background-image:url("${item.src}")`}
-		>
+		<Card class="flex aspect-square flex-col justify-end overflow-hidden">
+			<div class="relative flex-1">
+				<img
+					src={item.src}
+					alt={item.label}
+					class="absolute inset-0 h-full w-full object-cover"
+				/>
+			</div>
 			<Separator />
-			<CardFooter class="rounded-b-md bg-[#00000099] pt-4 text-white backdrop-blur-sm"
-				>{item.label}</CardFooter
-			>
+			<CardFooter class="rounded-b-md bg-[#00000099] pt-4 text-white backdrop-blur-sm">
+				{item.label}
+			</CardFooter>
 		</Card>
 	</DialogTrigger>
 	<DialogContent class="flex min-h-[70%] min-w-[70%] flex-col">
 		<DialogTitle>{item.label}</DialogTitle>
-		<div
-			class="flex-1 bg-cover bg-center bg-no-repeat"
-			style={`background-image: url("${item.src}")`}
-		></div>
+		<div class="flex-1 overflow-hidden">
+			<img
+				src={item.src}
+				alt={item.label}
+				class="h-full w-full object-contain"
+			/>
+		</div>
 		<DialogFooter>
 			<DialogClose>
 				<Button>Close</Button>
